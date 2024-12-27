22b6184418751f51faca43f84dd3300a80b2ad77
AnnaVo0 <annavo45@gmail.com>
2024-12-26 19:00:47 -0500

    customized the website more, adding in a placeholder image along with background color blocks

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 export default function Navbar() {
     return (
         <nav>
-            <ul className="min-w-max leading-8">
-                <li className="font-bold text-xl mb-1"><Link href="/">Anna Vo</Link></li>
+            <ul className="hidden sm:block min-w-max p-4 leading-8 rounded-2xl bg-orange-50">
+                <li className="font-bold text-lg mb-1"><Link href="#about">Anna Vo</Link></li>
                 <li><Link href="#projects">Projects</Link></li>
                 <li><Link href="#experience">Experience</Link></li>
                 <li><Link href="#contact">Contact</Link></li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import Navbar from './navbar';
+import Image from 'next/image';
 
 export default function App() {
   return (
-    <body className="overflow-x-hidden">
-        <div className="max-w-screen-lg w-screen mx-auto px-24 py-16 flex justify-between gap-16">
+    <body className="bg-orange-100 text-orange-950">
+        <div className="max-w-prose sm:max-w-screen-lg w-screen mx-auto px-4 py-16 sm:px-20 sm:flex sm:justify-between sm:gap-12">
             <Navbar />
-            <main>
-              <section>
+            <main className="px-8 py-12 rounded-2xl bg-orange-50">
+              <Image
+                className="rounded-full mx-auto mb-6"
+                src="/placeholder.jpg"
+                width={150}
+                height={150}
+                alt="Picture of me"
+              />
+              <section className="mb-16">
+                <h1 className="text-center text-2xl">Anna Vo</h1>
+              </section>
+              <section id="about">
                 <h1>About</h1>
                 <p>
                   Hi! I am a first-year computer science student @ UF (graduating May 2028).
