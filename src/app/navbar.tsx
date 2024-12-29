1c8c3dbe3f2cd4a0410691670e7e623b32c4cb3a
AnnaVo0 <annavo45@gmail.com>
2024-12-28 21:32:40 -0500

    finished and moved the contacts section to the top & added further customization to the website

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -5,10 +5,9 @@ export default function Navbar() {
     return (
         <nav>
             <ul className="hidden sm:block min-w-max p-6 leading-8 rounded-2xl text-orange-950 bg-orange-50">
-                <li className="font-bold text-lg mb-1"><Link href="#about">Anna Vo</Link></li>
-                <li><Link href="#projects">Projects</Link></li>
-                <li><Link href="#experience">Experience</Link></li>
-                <li><Link href="#contact">Contact</Link></li>
+                <li className="font-bold text-lg mb-1 hover:text-orange-900"><Link href="#page_top">Anna Vo</Link></li>
+                <li className="hover:text-tan"><Link href="#about">About</Link></li>
+                <li className="hover:text-tan"><Link href="#projects">Projects</Link></li>
             </ul>
         </nav>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,42 @@ export default function App() {
         <div className="max-w-prose sm:max-w-screen-lg mx-auto px-4 py-16 sm:px-20 sm:flex sm:justify-between sm:gap-10">
             <Navbar />
             <main className="text-orange-950 px-8 py-12 rounded-2xl bg-orange-50">
-              <section className="mb-16">
+              <section id="page_top" className="mb-16">
                 <Image
-                  className="rounded-full mx-auto mb-6"
+                  className="rounded-full mx-auto mb-6 transition ease-in-out hover:scale-105"
                   src="/placeholder.jpg"
                   width={150}
                   height={150}
                   alt="Picture of me"
                 />
                 <h1 className="text-center text-2xl mb-3">Anna Vo</h1>
-                <h2 className="text-center italic">First-year Computer Science Student @ UF May 2028</h2>
+                <h2 className="text-center italic">First-year Computer Science Student @ UF (May 2028)</h2>
+                <ul className="flex justify-center mt-6 gap-4">
+                  <li>
+                    <Link href="https://github.com/AnnaVo0" target="_blank">
+                      <Image
+                        className="justify-center transition ease-in-out hover:opacity-85"
+                        src="/github.svg"
+                        width={30}
+                        height={30}
+                        alt="Github logo"
+                      />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="https://www.linkedin.com/in/anna-n-vo" target="_blank">
+                      <Image
+                        className="justify-center transition ease-in-out hover:opacity-85"
+                        src="/linkedin.svg"
+                        width={30}
+                        height={30}
+                        alt="LinkedIn logo"
+                      />
+                    </Link>
+                  </li>
+                </ul>
               </section>
-              <section id="about">
+              <section id="about" className="mb-8">
                 <h1>About</h1>
                 <p>
                   Hi! I'm Anna and while I am still learning about the various fields in CS, I currently enjoy designing and creating new projects, such as this website, and combining my love for code and visual design together.
@@ -30,7 +54,7 @@ export default function App() {
               </section>
               <section id="projects">
                 <h1>Projects</h1>
-                <div>
+                <div className="my-8">
                   <Link href="https://replit.com/@annieV45/Game-Jam-Light-the-Tundra" target="_blank" className="inline-block">
                     <Image
                       className="rounded-md mb-6 ring ring-tan hover:ring-tan/50"
@@ -40,9 +64,14 @@ export default function App() {
                       alt="Picture of Light the Tundra's start screen"
                     />
                   </Link>
-                  <h2 className="font-bold">
-                    Light the Tundra
-                  </h2>
+                  <div id="light_the_tundra" className="flex gap-4 mb-4">
+                    <h2 className="font-bold">
+                      Light the Tundra
+                    </h2>
+                    <p className="text-xs my-auto font-bold text-tan">
+                      p5.js, p5.play
+                    </p>
+                  </div>
                   <p>
                     ​A 2D platformer game made for the SIP 2023 game jam in four days. 
                     The player must guide Lily, a lost girl with a dying lantern, to find spare lantern fuel.
@@ -58,30 +87,23 @@ export default function App() {
                       alt="Picture of me"
                     />
                   </Link>
-                  <h2 className="font-bold">
-                    Tear Catch
-                  </h2>
+                  <div id="tear_catch" className="flex gap-4 mb-4">
+                    <h2 className="font-bold">
+                      Tear Catch
+                    </h2>
+                    <p className="text-xs my-auto font-bold text-tan">
+                      p5.js, p5.play
+                    </p>
+                  </div>
                   <p>
                   A game based around helping my sister catch her tears before they can flood her desk.
                   The player must move a human, the catcher, to catch her tears before they can fall down from the top of the screen.
                   </p>
                 </div>
               </section>
-              <section id="experience">
-                <h1>Experience</h1>
-                <p>
-                  Placeholder.
-                </p>
-              </section>
-              <section id="contact">
-                <h1>Contact</h1>
-                <p>
-                  Placeholder.
-                </p>
-              </section>
               <section>
-                <p className="mt-12">
-                  This website was built by me using Next.js and Tailwind CSS!
+                <p className="mt-16 mb-0">
+                  This website was built by me using <b>Next.js</b> and <b>Tailwind CSS</b>!
                 </p>
               </section>
             </main>
